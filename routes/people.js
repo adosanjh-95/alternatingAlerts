2aa009ed810329d8cab2f90c078893dd964cda2a
amardeep <amardeep.dosanjh@and.digital>
2020-03-31 20:28:45 +0100

    Added basic edit endpoint

--- routes/people.js
+++ routes/people.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const {
   getPeople,
   createPerson,
-  deletePerson
+  deletePerson,
+  updatePerson
 } = require("../controllers/peopleController");
 
 router.route("/").get(getPeople);
@@ -11,5 +12,7 @@ router.route("/").get(getPeople);
 router.route("/").post(createPerson);
 
 router.route("/:id").delete(deletePerson);
+
+router.route("/:id").put(updatePerson);
 
 module.exports = router;
