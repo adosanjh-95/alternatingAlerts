b7565401ae6b120a80fe284fc5d08091460bcc43
amardeep <amardeep.dosanjh@and.digital>
2020-04-04 14:53:09 +0100

    Updated loading of .env and scripts

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const { MONGODB_URI } = require("./config");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true
