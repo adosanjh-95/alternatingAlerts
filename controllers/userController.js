6b03eafea2c171560cc90c21ea8c4433028cb360
amardeep <amardeep.dosanjh@and.digital>
2020-04-06 20:16:38 +0100

    Added login which returns the accessKey - to be updated to token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const { SUBSCRIPTION_KEY } = require("../config");
 
 exports.registerUser = async (req, res, next) => {
   const { username, password } = req.body;
@@ -28,6 +29,38 @@ exports.registerUser = async (req, res, next) => {
       data: { username }
     });
   } catch (err) {
+    return res.status(500).json({
+      success: false,
+      errors: err
+    });
+  }
+};
+
+exports.loginUser = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (user) {
+      const correctPassword = await bcrypt.compare(password, user.passwordHash);
+      if (correctPassword) {
+        return res.status(200).json({
+          username,
+          accessKey: SUBSCRIPTION_KEY
+        });
+      } else {
+        return res
+          .status(400)
+          .json("The user/password combination was incorrect");
+      }
+    } else {
+      return res
+        .status(404)
+        .json(`A user with username ${username} was not found`);
+    }
+  } catch (err) {
+    console.log(err);
     return res.status(500).json({
       success: false,
       errors: err
--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../src/server");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 const api = supertest(app);
 
@@ -19,20 +20,47 @@ describe("user model tests", () => {
     expect(response.body.success).toBe(true);
   });
 
-  it("a user cannot register with the same username that alreadt exists", async () => {
-    await api
-      .post("/user/register")
-      .send({ user: "test", password: "test" })
-      .expect(500);
-  });
-
-  it("a user cannot register if they do not send the correct fields", async () => {
+  it("a user cannot register with the same username that already exists", async () => {
     await User.create({ username: "test", password: "password" });
 
     await api
       .post("/user/register")
       .send({ username: "test", password: "test" })
       .expect(400);
+  });
+
+  it("a user cannot register if they do not send the correct fields", async () => {
+    await api
+      .post("/user/register")
+      .send({ user: "test", password: "test" })
+      .expect(500);
+  });
+
+  it("a registered user can login with the correct password", async () => {
+    const hash = await bcrypt.hash("password", 10);
+    await User.create({ username: "test", passwordHash: hash });
+
+    await api
+      .post("/user/login")
+      .send({ username: "test", password: "password" })
+      .expect(200);
+  });
+
+  it("a registered user cannot login with an incorrect password", async () => {
+    const hash = await bcrypt.hash("password", 10);
+    await User.create({ username: "test", passwordHash: hash });
+
+    await api
+      .post("/user/login")
+      .send({ username: "test", password: "akdkakda" })
+      .expect(400);
+  });
+
+  it("a non registered user cannot login", async () => {
+    await api
+      .post("/user/login")
+      .send({ username: "test", password: "akdkakda" })
+      .expect(404);
   });
 
   afterAll(async done => {
